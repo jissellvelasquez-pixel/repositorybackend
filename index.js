59962893b69eb1dcd9886efb447f5a755be07ea4
Jissell Velasquez Arapa <jissell.velasquez@tecsup.edu.pe>
2026-05-04 12:15:23 -0500

    Agregamos package.json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
- 
+require('dotenv').config();
+
+const http = require('http');
+
+function requestController(req, res) {
+  console.log('Bienvenidos al curso');
+  res.end('Servidor funcionando correctamente');
+}
+
+const server = http.createServer(requestController);
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, function () {
+  console.log("Aplicación corriendo en: " + PORT);
+}); 
